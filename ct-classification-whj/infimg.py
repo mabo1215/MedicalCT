c212e6b16d3b01a954db611c7c504b7cdb12aeb3
Darling-945 <66286966+Darling-945@users.noreply.github.com>
2022-02-13 00:09:33 +0800

    Add files via upload

--- ct-classification-whj/infimg.py
+++ ct-classification-whj/infimg.py
@@ -11,11 +11,11 @@ import math
 import torch
 from PIL import Image, ImageOps, ImageFilter
 from torchvision import transforms, models
+from utils.utils import *
 import matplotlib.pyplot as plt
-import statistics
+from sklearn.metrics import auc
 import seaborn as sns
 from sklearn.metrics import roc_curve
-from sklearn.metrics import roc_auc_score
 
 
 class Resize(object):
@@ -66,7 +66,9 @@ def load_checkpoint(filepath, model_name):
     elif model_name == "ShuffleNetV2":
         model = models.ShuffleNetV2()
     elif model_name == "resnet152":
-        model = models.resnet152(pretrained=True)
+        model = models.resnet152(pretrained=False)
+    elif model_name == 'SqueezeNet':
+        model = models.squeezenet1_1(pretrained=True)
     else:
         model = models.resnet101(pretrained=True)
     # model = checkpoint['model']  # 提取网络结构
@@ -77,50 +79,7 @@ def load_checkpoint(filepath, model_name):
     return model
 
 
-def plot_roc_curve(fpr, tpr):
-    plt.plot(fpr, tpr, color='orange', label='ROC')
-    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend()
-    plt.show()
-
-
-def result_csv(csv_url, truth, predict, model_name):
-    data = pd.read_csv(csv_url)
-    data.columns = ['id', 'labels']
-    dtp = data[data.id.str.contains(str(truth[0])) & data.labels.str.contains(str(predict[0]))]
-    dfn = data[data.id.str.contains(str(truth[0])) & data.labels.str.contains(str(predict[1]))]
-    dfp = data[data.id.str.contains(str(truth[1])) & data.labels.str.contains(str(predict[0]))]
-    dtn = data[data.id.str.contains(str(truth[1])) & data.labels.str.contains(str(predict[1]))]
-    tp = len(dtp)
-    fp = len(dfp)
-    tn = len(dtn)
-    fn = len(dfn)
-    tpr = tp / (tp + fn)
-    fpr = fp / (fp + tn)
-    # model_name = [str(model_name)]
-    acc = round((tp + tn) / (tp + fp + fn + tn), 3)
-    recall = round(tp / (tp + fn), 3)
-    precision = round(tp / (tp + fp), 3)
-    false_positive_rate = round(fp / (fp + tn + 0.01), 3)
-    positive_predictive_value = round(tp / (tp + fp + 0.01), 3)
-    negative_predictive_value = round(tn / (fn + tn + 0.01), 3)
-    result = pd.DataFrame({"model name": model_name,
-                           "Accuracy": acc,
-                           "Recall": recall,
-                           "Precision": precision,
-                           # "Variance": statistics.variance(),
-                           "False positive rate": false_positive_rate,
-                           "Positive predictive value": positive_predictive_value,
-                           "Negative predictive value": negative_predictive_value})
-
-    # return result
-    return result, tpr, fpr
-
-
-def predict(model, model_name):
+def predict(model, model_name, test_percent):
     # 读入模型
     model = load_checkpoint(model, model_name)
     print('..... Finished loading model! ......')
@@ -132,34 +91,45 @@ def predict(model, model_name):
     if dev == 1:
         model.cuda()
     pred_list, _id = [], []
-    for i in tqdm(range(len(imgs))):
-        img_path = imgs[i].strip()
-        # print(img_path)
-        _id.append(os.path.basename(img_path).split('.')[0])
-        img = Image.open(img_path).convert('RGB')
-        # print(type(img))
-        img = get_test_transform(mean, std, size=cfg.INPUT_SIZE)(img).unsqueeze(0)
+    # test_percent = 0.1
+    num = len(imgs)
+    list = range(num)
+    test = int(test_percent * num)
+    testlist = random.sample(list, test)
+    for i in tqdm(list):
+        # img_path = imgs[i].strip()
+        if i in testlist:
+            img_path = imgs[i].strip()
+            # print(img_path)
+            _id.append(os.path.basename(img_path).split('.')[0])
+            img = Image.open(img_path).convert('RGB')
+            # print(type(img))
+            img = get_test_transform(mean, std, size=cfg.INPUT_SIZE)(img).unsqueeze(0)
 
-        if dev == 1:
-            img = img.cuda()
-        with torch.no_grad():
-            out = model(img)
-        prediction = torch.argmax(out, dim=1).cpu().item()
-        pred_list.append(prediction)
+            if dev == 1:
+                img = img.cuda()
+            with torch.no_grad():
+                out = model(img)
+            prediction = torch.argmax(out, dim=1).cpu().item()
+            pred_list.append(prediction)
     return _id, pred_list
 
 
 if __name__ == "__main__":
     trained_model = cfg.TRAINED_MODEL
     model_name = cfg.model_name
-    save_path = './infdata/{}_result.csv'.format(model_name)
+
+    if not os.path.exists(cfg.BASE + '/infdata/'):
+        os.makedirs(cfg.BASE + '/infdata/')
+    if not os.path.exists(cfg.BASE + '/result/'):
+        os.makedirs(cfg.BASE + '/result/')
+    save_path = './result/{}_result.csv'.format(model_name)
+
     with open(cfg.TEST_LABEL_DIR, 'r') as f:
         imgs = f.readlines()
 
     # _id, pred_list = tta_predict(trained_model)
-    idx, pred_list = predict(trained_model, model_name)
-    # print(pred_list)
-    # print(idx)
+    idx, pred_list = predict(trained_model, model_name, test_percent=1)
     Truth = ['non', 'covid']
     Class_name = ['Normal', 'Pneumonia']
     pre_res = []
@@ -169,15 +139,20 @@ if __name__ == "__main__":
         else:
             pre_res.append(Class_name[1])
     # print(pre_res)
-    idx = list(idx)
+    # print(idx)
     submission = pd.DataFrame({'id': idx, 'labels': pre_res})
-    if not os.path.exists(cfg.BASE + '/infdata/'):
-        os.makedirs(cfg.BASE + '/infdata/')
     submission.to_csv(cfg.BASE + '/infdata/{}_submission.csv'
                       .format(model_name), index=False)
 
-    # result = result_csv(cfg.BASE + '/infdata/{}_submission.csv'.format(model_name), Truth, Class_name, model_name)
-    # result.to_csv(save_path,index=False, mode='a')
-    result, tpr, fpr = result_csv(cfg.BASE + '/infdata/{}_submission.csv'.format(model_name), Truth, Class_name,
-                                  model_name)
+    data = deal_csv(cfg.BASE + '/infdata/{}_submission.csv'.format(model_name), Truth, Class_name)
+    auc = plot_roc(data)
+    plot_cm(data)
+    acc, recall, precision, f1 = evaluate(data)
+
+    result = make_csv(model_name, acc, recall, precision, f1, auc)
+    result.to_csv(save_path, header=True, mode='a')
+
+    # result, tpr, fpr = result_csv(cfg.BASE + '/infdata/{}_submission.csv'.format(model_name), Truth, Class_name,
+    #                               model_name)
+    # data.to_csv(save_path, header=True, mode='a')
     # plot_roc_curve(fpr, tpr)
