941646ef9d8ae3f09425efd041bbcdb6d041cedd
mabo1215 <mabo1215@qq.com>
2022-02-14 00:50:43 +1300

    update
    update

--- ct-classification-whj/maketxt.py
+++ ct-classification-whj/maketxt.py
@@ -1,7 +1,7 @@
 import os
 import random
 import argparse
-
+from PIL import Image
 
 # rename file
 def file_rename(path, name, num, file_type):
@@ -10,27 +10,37 @@ def file_rename(path, name, num, file_type):
     filenames = os.listdir(path)
     for files in filenames:
         old_name = os.path.join(path, files)
-        if os.path.isdir(old_name):
-            continue
-        new_name = os.path.join(path, name + str(count + int(num)) + file_type)
-        os.rename(old_name, new_name)
-        count += 1
+        # print(old_name)
+        if not os.path.isdir(old_name):
+            # continue
+            new_name = os.path.join(path, name + str(count + int(num)) + file_type)
+            if os.path.isdir(new_name):
+                count += 1
+                new_name = os.path.join(path, name + str(count + int(num)) + file_type)
+            if old_name.endswith('.png'):
+                im1 = Image.open(old_name)
+                rgb_im = im1.convert('RGB')
+                rgb_im.save(new_name)
+                os.remove(old_name)
+            else:
+                os.rename(old_name, new_name)
+            count += 1
     print(str(count) + 'files renamed')
 
 
 # make txt file
-def write_txt(file_path, txt, test_percent):
+def write_txt(file_path, txt_path, test_percent):
     total = os.listdir(file_path)
     num = len(total)
     list = range(num)
     test = int(test_percent * num)
     testlist = random.sample(list, test)
-    f = open(txt, 'w')
+    f = open(txt_path, 'a+')
     for i in list:
         name = total[i]
         if i in testlist:
             out_path = file_path + '/' + name
-            print(out_path)
+            # print(out_path)
             f.write(out_path + '\n')
     f.close()
 
@@ -41,8 +51,8 @@ if __name__ == '__main__':
     parser.add_argument('--txt_name', '--t', type=str, default='', help='output file name')
     parser.add_argument('--start_name', '--n', type=str, default='', help='start name')
     parser.add_argument('--start_num', '--b', type=str, default='', help='start num')
-    parser.add_argument('--file_type', type=str, default='', help='file type such as:.jpg')
-    parser.add_argument('--options', '--o', type=str, default='', help='options rename or write')
+    parser.add_argument('--file_type', type=str, default='.jpg', help='file type such as:.jpg')
+    parser.add_argument('--options', '--o', type=str, default='renam', help='options rename or write')
     parser.add_argument('--percent', type=float, default=1, help='test image number percent')
 
     args = parser.parse_args()
